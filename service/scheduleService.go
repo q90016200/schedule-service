8131862c706f73326417e053a75ffd607f9b9860
q90016200 <q90016200@gmail.com>
2024-01-18 11:35:27 +0800

    fix rpc

--- service/scheduleService.go
+++ service/scheduleService.go
@@ -189,7 +189,10 @@ func CreateCronTask(id string, task *model.Job) {
 			em := empty.Empty{}
 			err := g.Invoke(context.Background(), task.Path, &em, &em)
 			log.Error(task.Name + " | " + task.Consul + task.Path + " | " + err.Error())
-			//defer g.Close()
+			defer func() {
+				g.Close()
+				rpcClients.Delete(taskId)
+			}()
 
 			break
 		}
